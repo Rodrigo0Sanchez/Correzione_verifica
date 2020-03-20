61e6be67af4de7db0ee6903022d1dd0f1c2f13cd
Rodrigo0Sanchez <sanchez.salinas.gianpier.rodrigo@iisgalvanimi.edu.it>
2020-03-20 15:55:18 +0000

    punto 2

--- auto-Sanchez/src/app/app.module.ts
+++ auto-Sanchez/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { DrivingListComponent } from './driving-list/driving-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrivingListComponent
   ],
   imports: [
     BrowserModule
